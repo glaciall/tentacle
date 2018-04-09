e3900c0684744a1ced3ce8020d9b0663e055bf0d
matrixy <matrixy@me.com>
2018-04-09 23:06:22 +0800

    数据压缩接口定义

---
+++ common/src/main/java/cn/org/hentai/tentacle/compress/BaseCompressProcessor.java
@@ -0,0 +1,9 @@
+package cn.org.hentai.tentacle.compress;
+
+/**
+ * Created by matrixy on 2018/4/9.
+ */
+public abstract class BaseCompressProcessor
+{
+    public abstract int[] compress(int[] sourceData);
+}
---
+++ common/src/main/java/cn/org/hentai/tentacle/compress/CompressUtil.java
@@ -0,0 +1,19 @@
+package cn.org.hentai.tentacle.compress;
+
+/**
+ * Created by matrixy on 2018/4/9.
+ */
+public final class CompressUtil
+{
+    /**
+     * 使用指定的压缩方法进行数据压缩
+     * @param method 压缩方法，如rle或huffman
+     * @param sourceData
+     * @return
+     */
+    public static int[] process(String method, int[] sourceData)
+    {
+        // TODO: 待实现具体压缩算法
+        return sourceData;
+    }
+}
