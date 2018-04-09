64e71593ba55fa9f4a3b72912a0e6a451780d5ca
matrixy <matrixy@me.com>
2018-04-09 23:08:13 +0800

    屏幕差异计算定义

---
+++ common/src/main/java/cn/org/hentai/tentacle/diff/Diff.java
@@ -0,0 +1,20 @@
+package cn.org.hentai.tentacle.diff;
+
+import cn.org.hentai.tentacle.graphic.Screen;
+
+/**
+ * Created by matrixy on 2018/4/9.
+ */
+public final class Diff
+{
+    /**
+     * 对两个屏幕的图像进行差异计算
+     * @param lastScreen 上一个屏幕的图像
+     * @param currentScreen 当前屏幕的图像
+     * @return
+     */
+    public static void calculate(Screen lastScreen, Screen currentScreen)
+    {
+        // TODO: 待实现
+    }
+}
