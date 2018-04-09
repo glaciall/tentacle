f8e1e6770463ab453c678bc192bc720fc26a4144
matrixy <matrixy@me.com>
2018-04-09 22:40:52 +0800

    本地电脑相关API

---
+++ common/src/main/java/cn/org/hentai/tentacle/system/LocalComputer.java
@@ -0,0 +1,43 @@
+package cn.org.hentai.tentacle.system;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
+/**
+ * Created by matrixy on 2018/4/9.
+ */
+public final class LocalComputer
+{
+    static Robot robot = null;
+
+    /**
+     * 创建整屏截图
+     * @return
+     */
+    public static BufferedImage captureScreen()
+    {
+        return robot.createScreenCapture(getScreenSize());
+    }
+
+    /**
+     * 获取屏幕分辨率
+     * @return
+     */
+    public static Rectangle getScreenSize()
+    {
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        return new Rectangle((int)screenSize.getWidth(), (int)screenSize.getHeight());
+    }
+
+    public static void init()
+    {
+        try
+        {
+            robot = new Robot();
+        }
+        catch(AWTException ex)
+        {
+            throw new RuntimeException("权限不足");
+        }
+    }
+}
