09e3269865d2eacde14f83def97a4cd4ecbf4601
matrixy <matrixy@me.com>
2018-04-10 23:11:29 +0800

    更改命名

--- common/src/main/java/cn/org/hentai/tentacle/diff/Diff.java
+++ common/src/main/java/cn/org/hentai/tentacle/diff/Diff.java
@@ -1,6 +1,6 @@
 package cn.org.hentai.tentacle.diff;
 
-import cn.org.hentai.tentacle.graphic.Screen;
+import cn.org.hentai.tentacle.graphic.Screenshot;
 
 /**
  * Created by matrixy on 2018/4/9.
@@ -9,11 +9,11 @@ public final class Diff
 {
     /**
      * 对两个屏幕的图像进行差异计算
-     * @param lastScreen 上一个屏幕的图像
-     * @param currentScreen 当前屏幕的图像
+     * @param lastScreenshot 上一个屏幕的图像
+     * @param currentScreenshot 当前屏幕的图像
      * @return
      */
-    public static void calculate(Screen lastScreen, Screen currentScreen)
+    public static void calculate(Screenshot lastScreenshot, Screenshot currentScreenshot)
     {
         // TODO: 待实现
     }
