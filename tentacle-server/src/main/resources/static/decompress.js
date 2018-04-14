9a672fe45ed5f3876dff57ef6634b5c0845caed6
matrixy <matrixy@me.com>
2018-04-14 22:03:27 +0800

    修正js版解压缩问题

--- tentacle-server/src/main/resources/static/decompress.js
+++ tentacle-server/src/main/resources/static/decompress.js
@@ -9,7 +9,7 @@ function decompress(method, compressedData, imageData)
     console.log(compressedData);
     var f = 0;
     var x = 0;
-    for (var i = (compressedData[0] & 0xff) * 3, k = 0; i < compressedData.length; )
+    for (var i = (compressedData[0] & 0xff) * 3 + 1, k = 0; i < compressedData.length; )
     {
         var rl = compressedData[i] & 0xff;
         var times = rl & 0x7f;
@@ -20,13 +20,14 @@ function decompress(method, compressedData, imageData)
             red = compressedData[index] & 0xff;
             green = compressedData[index + 1] & 0xff;
             blue = compressedData[index + 2] & 0xff;
-            if (x++ < 100) console.log('need colortable');
+            i += 2;
         }
         else
         {
             red = compressedData[i + 1] & 0xff;
             green = compressedData[i + 2] & 0xff;
             blue = compressedData[i + 3] & 0xff;
+            i += 4;
         }
 
         for (var s = 0; s < times; s++)
@@ -35,8 +36,6 @@ function decompress(method, compressedData, imageData)
             imageData.data[k++] = green;
             imageData.data[k++] = blue;
             imageData.data[k++] = 0xff;
-            if (f++ < 100) console.log(((red << 16) | (green << 8) || blue).toString(16));
         }
-        i += (rl & 0x80) > 0 ? 2 : 4;
     }
 }
