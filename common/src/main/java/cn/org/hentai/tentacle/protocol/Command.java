7de6974b73a31014e4b2867ea1a90974191d2005
matrixy <matrixy@me.com>
2018-04-19 22:23:46 +0800

    增加注释

--- common/src/main/java/cn/org/hentai/tentacle/protocol/Command.java
+++ common/src/main/java/cn/org/hentai/tentacle/protocol/Command.java
@@ -5,15 +5,15 @@ package cn.org.hentai.tentacle.protocol;
  */
 public final class Command
 {
-    public static final byte COMMON_RESPONSE = 0x00;
-    public static final byte HEARTBEAT = 0x01;
-    public static final byte CONTROL_REQUEST = 0x02;
-    public static final byte CONTROL_RESPONSE = 0x03;
-    public static final byte CLOSE_REQUEST = 0x04;
-    public static final byte CLOSE_RESPONSE = 0x05;
-    public static final byte HID_COMMAND = 0x06;
-    public static final byte SCREENSHOT = 0x07;
+    public static final byte COMMON_RESPONSE = 0x00;                // 常规应答
+    public static final byte HEARTBEAT = 0x01;                      // 心跳包
+    public static final byte CONTROL_REQUEST = 0x02;                // 请求控制
+    public static final byte CONTROL_RESPONSE = 0x03;               // 请求控制包的应答
+    public static final byte CLOSE_REQUEST = 0x04;                  // 关闭控制
+    public static final byte CLOSE_RESPONSE = 0x05;                 // 关闭控制包的应答（未启用）
+    public static final byte HID_COMMAND = 0x06;                    // 人机接口指令
+    public static final byte SCREENSHOT = 0x07;                     // 屏幕截图
 
-    public static final byte TYPE_MOUSE = 0x01;
-    public static final byte TYPE_KEYBOARD = 0x02;
+    public static final byte TYPE_MOUSE = 0x01;                     // 类型：鼠标
+    public static final byte TYPE_KEYBOARD = 0x02;                  // 类型：键盘
 }
