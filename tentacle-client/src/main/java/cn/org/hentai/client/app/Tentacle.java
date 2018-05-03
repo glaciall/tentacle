e0cd3817562f7e83bfacab15d7033acf4092a368
matrixy <matrixy@me.com>
2018-05-04 00:05:47 +0800

    去图形化

--- tentacle-client/src/main/java/cn/org/hentai/client/app/Tentacle.java
+++ tentacle-client/src/main/java/cn/org/hentai/client/app/Tentacle.java
@@ -1,28 +1,19 @@
 package cn.org.hentai.client.app;
 
 import cn.org.hentai.client.client.Client;
-import cn.org.hentai.client.test.ScreenCanvas;
 import cn.org.hentai.tentacle.compress.RLEncoding;
 import cn.org.hentai.tentacle.hid.KeyMapping;
 import cn.org.hentai.tentacle.system.LocalComputer;
 import cn.org.hentai.tentacle.util.Configs;
-
-import javax.swing.*;
-import java.awt.*;
 
 /**
  * Created by matrixy on 2018/4/9.
  */
 public class Tentacle
 {
-    JFrame frame;
-    private static final int WINDOW_WIDTH = 320;
-    private static final int WINDOW_HEIGHT = 240;
-
     public Tentacle()
     {
         initCore();
-        initWindow();
     }
 
     // 核心模块初始化，提高运行中的性能
@@ -39,23 +30,6 @@ public class Tentacle
         LocalComputer.init();
 
         new Client().start();
-    }
-
-    // 窗口UI初始化
-    private void initWindow()
-    {
-        Rectangle screenSize = LocalComputer.getScreenSize();
-
-        frame = new JFrame();
-        frame.setTitle("Tentacle Desktop");
-
-        JPanel panel = new JPanel();
-        frame.getContentPane().add(panel);
-        frame.setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
-        frame.setLocation((int)screenSize.getWidth() - WINDOW_WIDTH, (int)screenSize.getHeight() - WINDOW_HEIGHT - 50);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        // frame.setAlwaysOnTop(true);
-        frame.setVisible(true);
     }
 
     public static void main(String[] args) throws Exception
